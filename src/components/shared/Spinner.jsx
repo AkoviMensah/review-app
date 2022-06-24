5c58579871c3905bb6fb9ae5a2b6df0ea26db682
Akovi Mensah <akovi66@gmail.com>
2022-06-24 12:35:17 -0400

    updated shared components

---
+++ src/components/shared/Spinner.jsx
@@ -0,0 +1,9 @@
+function Spinner() {
+    return (
+        <img src={"../assets/spinner.gif"}
+        alt="Loading"
+        style={{ width: "100px", diplay: block, margin: "auto"}}/>
+    );
+};
+
+export default Spinner;
