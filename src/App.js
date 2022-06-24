ed2f506e4947da64649e24ae9b5ed4d150ca1f4c
Akovi Mensah <akovi66@gmail.com>
2022-06-24 10:39:18 -0400

    removed unused filed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import './App.css';
 import Header from './components/Header'
 import Review from './components/Review';
 
 function App() {
   return (
-    <>
-    <Header />
-    <div className="container">
-      <h1 >feedback</h1>
+    <div>
+      <Header/>
+      <Review/>
     </div>
-    <Review/>    
-    </>
+    
     
   );
 }
